3612b88e2d1f8bf41bc19be5de7ab36ba5caf185
tejaram11 <tejarammuramalla11@gmail.com>
2024-03-20 11:29:55 +0530

    training correction

--- train.py
+++ train.py
@@ -32,7 +32,7 @@ from write_csv_for_making_dataset import write_csv
 
 learning_rate=0.05
 step_size=20
-num_epochs=50
+num_epochs=100
 
 margin = 0.4
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
