e63d5ac0815fc0aa675ccab02ca4786267d3700d
tejaram11 <tejarammuramalla11@gmail.com>
2024-03-22 23:27:25 +0530

    resnet modification

--- data_loader.py
+++ data_loader.py
@@ -160,7 +160,7 @@ class TripletFaceDataset(Dataset):
         anc_img = os.path.join(self.root_dir, str(pos_class), str(anc_id)+'.jpg')
         pos_img = os.path.join(self.root_dir, str(pos_class), str(pos_id)+'.jpg')
         neg_img = os.path.join(self.root_dir, str(neg_class), str(neg_id)+'.jpg')
-
+        print(anc_img)
         # Modified to open as PIL image in the first place
         anc_img = self.mtcnn(io.imread(anc_img))[0]
         #print("inside data loader",anc_img.shape)
