8b4b1c02309f157bd64e00a7614c2660ed31f7db
tejaram11 <tejarammuramalla11@gmail.com>
2024-03-26 08:31:59 +0530

    pins unlearner

--- train_fc.py
+++ train_fc.py
@@ -86,7 +86,7 @@ valid_ds=casia_dataset(root_dir="/kaggle/input/cplfw/aligned",
 valid_loader = DataLoader(valid_ds, batch_size=1024, shuffle=True)
 
 # Step 5: Training loop
-num_epochs = 200
+num_epochs = 500
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
 best_acc= 0.0
